d3f3c2460f813db81efc00986531693a066dccf5
allen <qinglx@yeah.net>
2017-06-03 15:56:31 +0800

    Initial commit

---
+++ library/src/main/java/com/allen/lib/views/TrapezoidView.java
@@ -0,0 +1,24 @@
+package com.allen.lib.views;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by allen on 2017/6/3.
+ */
+
+public class TrapezoidView extends View {
+    public TrapezoidView(Context context) {
+        super(context);
+    }
+
+    public TrapezoidView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TrapezoidView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
